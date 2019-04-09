33eccd0994dd14ec37883ebdcb28db8222c60180
xuejuhui <xuejuhui@hotmail.com>
2019-04-09 11:19:24 -0400

    api wrapper for all api request and dispatch

--- frontend/src/actions/dishActions.js
+++ frontend/src/actions/dishActions.js
@@ -1,44 +1,27 @@
 import { GET_USER_DISHES, ADD_USER_DISHES } from "../actions/types";
-import { enqueueSnackbar } from "./alertActions";
-import apiClient from '../utils/api/apiClient';
+import { apiRequest } from "../utils/api/apiWrapper";
 export { getUserDishes, addUserDishes };
-
 
 function getUserDishes() {
   return async dispatch => {
-    try {
-      const getDishesResponse = await apiClient.get(
-        "/api/dishes/userdishes"
-      );
-      dispatch({
-        type: GET_USER_DISHES,
-        payload: getDishesResponse.data
-      });
-      dispatch(
-        enqueueSnackbar({ message: "Loaded", options: { variant: "success" } })
-      );
-    } catch (error) {
-      console.log(error);
-    }
+    apiRequest(
+      { url: "/api/dishes/userdishes", method: "GET" },
+      GET_USER_DISHES,
+      "Loaded"
+    )(dispatch);
   };
 }
 
-function addUserDishes({dishName, description}) {
+function addUserDishes({ dishName, description }) {
   return async dispatch => {
-    try {
-      const addDishesResponse = await apiClient.post(
-        "/api/dishes/userdishes",
-        {dishName,description}
-      );
-      dispatch({
-        type: ADD_USER_DISHES,
-        payload: addDishesResponse.data
-      });
-      dispatch(
-        enqueueSnackbar({ message: "added", options: { variant: "success" } })
-      );
-    } catch (error) {
-      console.log(error);
-    }
+    apiRequest(
+      {
+        url: "/api/dishes/userdishes",
+        method: "POST",
+        data: { dishName, description }
+      },
+      ADD_USER_DISHES,
+      "Added"
+    )(dispatch);
   };
 }
---
+++ frontend/src/utils/api/apiWrapper.js
@@ -0,0 +1,27 @@
+import apiClient from "./apiClient";
+import { enqueueSnackbar } from "../../actions/alertActions";
+
+const apiRequest = ({ url, ...apiOptions }, dispatchType, message = "") => {
+  return async dispatch => {
+    try {
+      const response = await apiClient({ url, ...apiOptions });
+      dispatch({
+        type: dispatchType,
+        payload: response.data
+      });
+      console.log(response);
+      dispatch(
+        enqueueSnackbar({
+          message,
+          options: { variant: "success" }
+        })
+      );
+    } catch (e) {
+      dispatch(
+        enqueueSnackbar({ message: `${e}`, options: { variant: "error" } })
+      );
+    }
+  };
+};
+
+export { apiRequest };
