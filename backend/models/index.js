97376aa39937393bf86d8c6af64daa394448bab8
xuejuhui <xuejuhui@hotmail.com>
2019-05-17 18:01:33 -0400

    add pm2 to project

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("app.dbConfig.host");
-
+console.log(`currently using this ${db}`);
 mongoose
   .connect(
     db,
---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  apps: [
+    {
+      name: "API",
+      cwd: "backend/",
+      script: "bin/www",
+
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      instance_var: "INSTANCE_ID",
+      autorestart: true,
+      max_restarts: 5,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: "node",
+      host: "212.83.163.1",
+      ref: "origin/master",
+      repo: "git@github.com:repo.git",
+      path: "/var/www/production",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production"
+    }
+  }
+};
